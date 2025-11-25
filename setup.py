a0ba53cf06319808c10ae6eb753772cbf572ed84
Han Bolun <Bolun.Han@outlook.com>
2025-11-25 14:49:19 +0800

    updated setup script

--- setup.py
+++ setup.py
@@ -1,39 +1,42 @@
-import codecs
 import os
+from pathlib import Path
 
-from setuptools import setup
+from Cython.Build import cythonize
+from setuptools import setup, Extension
 
 
 def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    # intentionally *not* adding an encoding option to open, See:
-    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
-    with codecs.open(os.path.join(here, rel_path).__str__(), 'r') as fp:
-        return fp.read()
+    here = Path(__file__).resolve().parent
+    return (here / rel_path).read_text(encoding="utf-8")
 
 
 def get_version(rel_path):
     for line in read(rel_path).splitlines():
         if line.startswith('__version__'):
-            # __version__ = "0.9"
             delim = '"' if '"' in line else "'"
             return line.split(delim)[1]
     raise RuntimeError("Unable to find version string.")
 
 
+cython_extension = [
+    Extension(
+        name="decision_graph.decision_tree.capi.c_abc",
+        sources=["decision_graph/decision_tree/capi/c_abc.pyx"],
+        # define_macros=[("CYTHON_TRACE", "1"), ('CYTHON_USE_SYS_MONITORING', '1'), ('CYTHON_TRACE_NOGIL', '1')],
+    ),
+    Extension(
+        name="decision_graph.decision_tree.capi.c_collection",
+        sources=["decision_graph/decision_tree/capi/c_collection.pyx"],
+    ),
+    Extension(
+        name="decision_graph.decision_tree.capi.c_node",
+        sources=["decision_graph/decision_tree/capi/c_node.pyx"],
+    ),
+]
+
+ext_modules = cythonize(cython_extension, compiler_directives={"language_level": "3"})
+
 setup(
-    version=get_version(os.path.join('decision_graph', '__init__.py')),
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown",
-    extras_require={
-        "docs": [
-            "sphinx",
-            "sphinx-rtd-theme",
-            "sphinx-autodoc-typehints"
-        ],
-        "visualization": [
-            "pyvis",
-            "networkx"
-        ]
-    },
+    name="PyDecisionGraph",
+    ext_modules=ext_modules,
 )

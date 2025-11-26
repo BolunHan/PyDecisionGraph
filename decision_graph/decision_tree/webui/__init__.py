32bfa2694da40b2332ee451289a3541c095ecd9d
Han Bolun <Bolun.Han@outlook.com>
2025-11-26 14:42:03 +0800

    added with_eval to visible signature

--- decision_graph/decision_tree/webui/__init__.py
+++ decision_graph/decision_tree/webui/__init__.py
@@ -8,13 +8,13 @@ from .app import DecisionTreeWebUi
 from .. import LogicNode
 
 
-def show(root: LogicNode, **kwargs):
+def show(root: LogicNode, with_eval: bool = True, **kwargs):
     _app = DecisionTreeWebUi(
         host=kwargs.get('host', "127.0.0.1"),
         port=kwargs.get('port', 5000),
         debug=kwargs.get('debug', False)
     )
-    _app.show(root, **kwargs)
+    _app.show(root, with_eval=with_eval, **kwargs)
 
 
 def to_html(root: LogicNode, file_name: str, with_eval: bool = True):
